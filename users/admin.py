243fb1e5b30617556640dc501fa573ff244d6d41
juniorpeves <juniorpeves@gmail.com>
2022-03-10 20:01:48 -0500

    Creación de Signup

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Profile, Country, House, Patronus
 
 # Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Country, CountryAdmin)
+
+class HouseAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(House, HouseAdmin)
+
+class PatronusAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Patronus, PatronusAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
